ae7e7f3635e12acec03624d20185f2aeef9500fa
김민수 <white@swichee.me>
2020-12-23 10:51:13 +0900

    [algo]  day2 solution (#2)

---
+++ Minsu/programmers/day2/42889.py
@@ -0,0 +1,32 @@
+# 실패율
+
+
+def solution(N, stages):
+
+    # stages 에 있는 값들의 개수 구하기
+    stage = {}
+    for i in stages:
+        stage[i] = stage.get(i, 0) + 1
+
+    # 실패율
+    failure = [0]*N
+    num = len(stages)
+    for i in range(1, N+1):
+        if stage.get(i, 0) != 0:    # 달라진 부분
+            failure[i-1] = stage[i]/num
+            num -= stage[i]
+
+    # 정렬
+    answer = []
+    num = len(failure)
+    for i, item in enumerate(failure):
+        m = item
+        idx = i
+        for j, _item in enumerate(failure[::-1]):
+            if _item >= m:
+                m = _item
+                idx = num - 1-j
+        answer.append(idx+1)
+        failure[idx] = -1
+
+    return answer
