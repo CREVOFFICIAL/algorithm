790784e5cd42ae666902afc09b17d94bd6d107fa
김민수 <rnfprhf60@gmail.com>
2021-05-03 23:42:52 +0900

    [dev] day25 solution (#28)

---
+++ Minsu/programmers/day25/12913.py
@@ -0,0 +1,13 @@
+# 땅따먹기
+
+def solution(land):
+    n = len(land)-1
+    # print(n)
+    for i in range(n):
+        print('i', i)
+        print(land[i+1][0])
+        land[i+1][0] += max(land[i][1], land[i][2], land[i][3])
+        land[i+1][1] += max(land[i][0], land[i][2], land[i][3])
+        land[i+1][2] += max(land[i][0], land[i][1], land[i][3])
+        land[i+1][3] += max(land[i][0], land[i][1], land[i][2])
+    return max(land[n][0], land[n][1], land[n][2], land[n][3])
